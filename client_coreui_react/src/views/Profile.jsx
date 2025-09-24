f2bf2b0ace24cd9c8d3eec871529aeabec47d532
MED SALEM GHALASS <msghalas@gmail.com>
2025-09-24 12:19:04 +0000

    profile page

--- client_coreui_react/src/views/Profile.jsx
+++ client_coreui_react/src/views/Profile.jsx
@@ -1,7 +1,91 @@
-import React from 'react'
+import {
+  CAvatar,
+  CBadge,
+  CButton,
+  CCard,
+  CCardBody,
+  CCardLink,
+  CCardSubtitle,
+  CCardText,
+  CCardTitle,
+  CCol,
+  CRow,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { useAuth } from '../context/AuthContext'
+import avatar8 from './../assets/images/avatars/8.jpg'
+import { cilPencil } from '@coreui/icons'
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>
+  const { user } = useAuth()
+  return (
+    <CRow className="">
+      <CCol md={4}>
+        <CCard className="mt-1">
+          <CCardBody>
+            <div className="d-flex align-items-top justify-content-between">
+              <div>
+                <CAvatar
+                  src={avatar8}
+                  className="border border-primary border-2 p-1 col-sm mb-1"
+                  style={{ height: '100px', width: '100px' }}
+                />
+              </div>
+
+              <div>
+                <CButton
+                  size="sm"
+                  color="primary"
+                  variant="outline"
+                  className="rounded-pill"
+                  onClick={() => {
+                    // setEntity(initialVal)
+                    // setVisible(!visible)
+                    // setOperation('create')
+                  }}
+                >
+                  <CIcon icon={cilPencil} />
+                </CButton>
+              </div>
+            </div>
+            <CCardTitle className="text-uppercase">
+              <CBadge textBgColor="light" shape="rounded-pill">
+                <span className="text-uppercase">
+                  {user?.name} {user?.lastName}
+                </span>
+              </CBadge>
+            </CCardTitle>
+            <CCardSubtitle className="mb-2 text-body-secondary text-uppercase">
+              <CBadge color="info" shape="rounded-pill">
+                <span className="text-uppercase">{user?.role}</span>
+              </CBadge>
+            </CCardSubtitle>
+            <CCardText>
+              <CBadge color="secondary" shape="rounded-pill">
+                <span className="">{user?.email}</span>
+              </CBadge>
+            </CCardText>
+            <CCardLink href="#">Card link</CCardLink>
+            <CCardLink href="#">Another link</CCardLink>
+          </CCardBody>
+        </CCard>
+      </CCol>
+      <CCol md={8}>
+        <CCard className="mt-1">
+          <CCardBody>
+            <CCardTitle>Card title</CCardTitle>
+            <CCardSubtitle className="mb-2 text-body-secondary">Card subtitle</CCardSubtitle>
+            <CCardText>
+              Some quick example text to build on the card title and make up the bulk of the card's
+              content.
+            </CCardText>
+            <CCardLink href="#">Card link</CCardLink>
+            <CCardLink href="#">Another link</CCardLink>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  )
 }
 
 export default ProfilePage
