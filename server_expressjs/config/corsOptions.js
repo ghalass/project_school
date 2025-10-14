17f7c62f49953ba76890374dbd14ff1fe82f7314
MED SALEM GHALASS <msghalas@gmail.com>
2025-10-14 11:23:22 +0000

    deploy with fix cookies

--- server_expressjs/config/corsOptions.js
+++ server_expressjs/config/corsOptions.js
@@ -1,11 +1,13 @@
 // corsOptions.js
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "http://localhost:5173"], // ✅ Spécifiez vos URLs frontend
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://school.ghalass.com", // ajout pour production
+  ],
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
-
-export default corsOptions;
