4fd96a76aecfeb65d46631553478a665275b4205
MED SALEM GHALASS <msghalas@gmail.com>
2025-10-14 10:28:15 +0000

    deploy add /api to server.js

--- server_expressjs/server.js
+++ server_expressjs/server.js
@@ -60,10 +60,10 @@ app.use("/", express.static(path.join(__dirname, "public"))); // static files
 app.use(logger);
 
 // USE ROUTES
-app.use("/", rootRoutes);
-app.use("/user", userRoutes);
-app.use("/auth", authRoutes);
-app.use("/dashboard", dashboardRoutes);
+app.use("/api/", rootRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 // 404 ROUTE
 app.all("*", (req, res) => {
